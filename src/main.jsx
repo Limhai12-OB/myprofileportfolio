413c4b56c7baf765030ea43e4742f16730683179
Limhai12-OB <trylimhai@gmail.com>
2025-05-22 23:12:10 +0700

    TEST

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import HeadPage from "./components/headpage";
-import AboutMe from "./components/aboutme";
+// import AboutMe from "./components/aboutme";
 import ProjectSec from "./components/myproject";
 import ContactSec from "./components/contactme";
 import FooterSec from "./components/footer";
@@ -10,7 +10,7 @@ import FooterSec from "./components/footer";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HeadPage />
-    <AboutMe />
+    {/* <AboutMe /> */}
     <ProjectSec />
     <ContactSec />
     <FooterSec />
